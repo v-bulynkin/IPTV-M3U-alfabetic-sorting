6e3c2b2f7c53e1d208e7d4698d6a7f585339ece4
v-bulynkin <56349712+v-bulynkin@users.noreply.github.com>
2020-11-21 12:03:15 +0300

    Add files via upload

---
+++ IPTV-M3U-alfabetic-sorting.ps1
@@ -0,0 +1,28 @@
+﻿$url = (Read-Host "Enter an IPTV playlist URL").trim()
+$file = "$env:userprofile\Downloads\$(([uri]$url).segments[-1])"
+curl $url -OutFile "$file"
+
+$l = ((gc "$file" -encoding utf8) -split '#EXTINF:-1 ,' -notmatch "EXTM3U|^$").Trim()
+$names,$links = $l.where({$_ -notmatch "^https?://"}, 'split')
+
+# Making ordered object
+$data = @()
+$c = 0
+
+$names|% {
+    $obj = [pscustomobject]@{
+    Name = $_
+    Link = $links[$c]
+    }
+$c++
+$data += $obj
+}
+
+$data = $data |sort Name
+
+# Output
+"#EXTM3U" |Out-File "$file" -Encoding utf8 -Force -Confirm:$false
+
+$data |% {
+"#EXTINF:-1 ," + $_.name + "`n" + $_.link |Out-File "$file" -Encoding utf8 -Append
+}
